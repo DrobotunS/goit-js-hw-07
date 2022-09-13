329cd95ccaed155b753ae0a60684da34d08275b5
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-13 23:08:53 +0300

    add3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
-
-const basicLightbox = require('basiclightbox')
-import * as basicLightbox from 'basiclightbox'
+const divGalleryRef = document.querySelector('.gallery');
+const galleryImage = createGalleryImage(galleryItems);
+divGalleryRef.insertAdjacentHTML('beforeend', galleryImage);
+divGalleryRef.addEventListener('click', onGalleryClick);
+function createGalleryImage(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+      `;
+    }).join('');
+    return galleryImages;
+};
+console.log(onGalleryClick);
+function onGalleryClick (evt) {
+    evt.preventDefault();
+console.log(evt.target)
+const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+if (!isGalleryImageEl) {
+    return;
+}
+}
